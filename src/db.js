11cbe8b87b7bb1becb8e73de8ed600328451b906
joanafalcaos <142123405+joanafalcaos@users.noreply.github.com>
2025-06-05 23:33:08 +0000

    subindo projeto

---
+++ src/db.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT || 5432,
+  ssl: { rejectUnauthorized: false },
+});
+
+module.exports = pool;
