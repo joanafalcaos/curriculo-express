e12c49a9e2cd648704fcaee1616ec67ee633b6e0
joanafalcaos <142123405+joanafalcaos@users.noreply.github.com>
2025-06-06 00:06:26 +0000

    fix env

--- src/db.js
+++ src/db.js
@@ -1,12 +1,11 @@
-// db.js
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: 5432,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT || 5432,
   ssl: {
     rejectUnauthorized: false,
   },
